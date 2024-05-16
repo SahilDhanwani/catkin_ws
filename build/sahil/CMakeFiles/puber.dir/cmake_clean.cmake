a9a93559f625435751c6cba33d8ae7c36e5bcb2d
Sahil Dhanwani <sahildhanwani291203@gmail.com>
2024-05-16 13:49:35 +0530

    Working fine

---
+++ build/sahil/CMakeFiles/puber.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/puber.dir/src/pub.cpp.o"
+  "puber"
+  "puber.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/puber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
