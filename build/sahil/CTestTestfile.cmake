fe99ece6974adcae9fa88efd395f47f02cb859ef
Sahil Dhanwani <sahildhanwani291203@gmail.com>
2024-05-16 19:46:54 +0530

    WS cleaned

--- build/sahil/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sahil/catkin_ws/src/sahil
-# Build directory: /home/sahil/catkin_ws/build/sahil
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/turtle_play/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sahil/catkin_ws/src/turtle_play
-# Build directory: /home/sahil/catkin_ws/build/turtle_play
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
