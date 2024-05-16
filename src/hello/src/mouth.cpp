5b3fb231ae2340a19cf0e9ed11f2a15477aacb1f
Sahil Dhanwani <sahildhanwani291203@gmail.com>
2024-05-16 20:26:55 +0530

    Done with controlling the turtle via publisher node in cpp

--- src/hello/src/mouth.cpp
+++ src/hello/src/mouth.cpp
@@ -1,29 +1,23 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
-
-#include <sstream>
+#include "geometry_msgs/Twist.h"
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "talker");
+  ros::init(argc, argv, "mouth");
 
   ros::NodeHandle n;
 
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+  ros::Publisher chatter_pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
 
   ros::Rate loop_rate(10);
 
   int count = 0;
   while (ros::ok())
   {
-    std_msgs::String msg;
+    geometry_msgs::Twist msg;
 
-    std::stringstream ss;
-    ss << "hello world " << count;
-    msg.data = ss.str();
+    msg.linear.x = 1.0;
 
-    ROS_INFO("%s", msg.data.c_str());
-    
     chatter_pub.publish(msg);
 
     ros::spinOnce();
@@ -31,7 +25,6 @@ int main(int argc, char **argv)
     loop_rate.sleep();
     ++count;
   }
-
 
   return 0;
 }
